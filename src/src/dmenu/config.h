1a322346d5ed4ecd7a57a495fec8da36d5927105
Okan Akyuz <okanakyuz@okanakyuz.com>
2025-06-13 22:02:23 +0000

    Apply patch to dmenu for XResource

--- src/src/dmenu/config.h
+++ src/src/dmenu/config.h
@@ -3,11 +3,11 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
+static char *fonts[] = {
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
+static char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
