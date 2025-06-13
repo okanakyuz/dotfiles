c8cfee62b1b679012dbea18986c94ce0ad917f06
Okan Akyuz <okanakyuz@okanakyuz.com>
2025-06-13 21:45:43 +0000

    Modify starydwm script for xrdb

--- src/.config/startdwm.sh
+++ src/.config/startdwm.sh
@@ -7,6 +7,20 @@ change_wallpaper ()
 	do 	
 		wallpaper=$(find ~/.config/wallpapers/ -type f | shuf -n 1)
 		feh --bg-scale $wallpaper ;
+
+		wal -i $wallpaper ;
+
+
+		echo "dwm.normbordercolor:  $(grep 'norm_border' ~/.cache/wal/colors-wal-dwm.h | awk -F'"' '{print $2}')" >> ~/.cache/wal/colors.Xresources ;
+		echo "dwm.normbgcolor:  $(grep 'norm_bg' ~/.cache/wal/colors-wal-dwm.h | awk -F'"' '{print $2}')" >> ~/.cache/wal/colors.Xresources ;
+		echo "dwm.normfgcolor:  $(grep 'norm_fg' ~/.cache/wal/colors-wal-dwm.h | awk -F'"' '{print $2}')" >> ~/.cache/wal/colors.Xresources ;
+		echo "dwm.selbordercolor:  $(grep 'sel_border' ~/.cache/wal/colors-wal-dwm.h | awk -F'"' '{print $2}')" >> ~/.cache/wal/colors.Xresources ;
+		echo "dwm.selbgcolor:  $(grep 'sel_bg' ~/.cache/wal/colors-wal-dwm.h | awk -F'"' '{print $2}')" >> ~/.cache/wal/colors.Xresources ;
+		echo "dwm.selfgcolor:  $(grep 'sel_fg' ~/.cache/wal/colors-wal-dwm.h | awk -F'"' '{print $2}')" >> ~/.cache/wal/colors.Xresources ;
+
+		xrdb -merge ~/.cache/wal/colors.Xresources ;
+
+
 		sleep 60 ;
 	done
 }
