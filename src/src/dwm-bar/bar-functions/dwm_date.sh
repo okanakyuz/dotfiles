3406ed5c54201c0d3a227e338b54d35772e6acc0
Okan Akyuz <okanakyuz@okanakyuz.com>
2025-06-18 10:23:45 +0000

    Adding dwm-bar

---
+++ src/src/dwm-bar/bar-functions/dwm_date.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# A dwm_bar function that shows the current date and time
+# GNU GPLv3
+
+# Date is formatted like like this: "[Mon 01-01-00 00:00:00]"
+dwm_date () {
+    printf "%s" "$SEP1"
+    if [ "$IDENTIFIER" = "unicode" ]; then
+        printf " %s" "$(date "+%a %d-%m-%y %T")"
+    else
+        printf "DAT %s" "$(date "+%a %d-%m-%y %T")"
+    fi
+    printf "%s\n" "$SEP2"
+}
+
+dwm_date
