ed6fd3ca684b18405541db4d6740999ed5cb6e1f
Okan Akyuz <okanakyuz@okanakyuz.com>
2025-06-18 10:27:43 +0000

    Adding install steps for Arch

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+sudo pacman -Syu --noconfirm
+sudo pacman -Sy --noconfirm base-devel git xorg-server xorg-xinit xorg-xrandr libx11 libxft libxinerama ttf-firacode-nerd python-pywal xorg-xrdb xorg-xsetroot 
+sudo pacman -Sy --noconfirm zsh exa bat fzf tmux ranger fd ripgrep htop lazygit 
+
 stow src
 stow picom
-~/src/buildall.sh
