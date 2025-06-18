d347fdb3deaed0a67def23938356b35fb069025b
Okan Akyuz <okanakyuz@okanakyuz.com>
2025-06-18 10:18:37 +0000

    Create config coppy

--- src/src/dwm/config.def.h
+++ src/src/dwm/config.def.h
@@ -10,8 +10,8 @@ static const unsigned int gappov    = 30;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "FiraCode Nerd Font:size=14:antialias=true:autohint=true" };
+static const char dmenufont[]       = "FiraCode Nerd Font:size=14:antialias=true:autohint=true";
 //static const char col_gray1[]       = "#222222";
 //static const char col_gray2[]       = "#444444";
 //static const char col_gray3[]       = "#bbbbbb";
@@ -80,7 +80,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_border, "-sf", sel_fg, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st" , NULL };
+
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
