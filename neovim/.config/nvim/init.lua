2d16c3d195b19ab19b22f9e56d3827a4f0fca585
Okan <okanakyuz@okanakyuz.com>
2025-06-20 17:44:58 +0300

    Update init.lua

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,21 +1,3 @@
-vim.opt.expandtab = false      
-vim.opt.tabstop = 4           
-vim.opt.shiftwidth = 4        
-vim.opt.softtabstop = 4
-vim.opt.number = true         
-vim.opt.relativenumber = true 
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("plugins")
+require("core.options")
+require("core.keymaps")
+require("core.lazy")
