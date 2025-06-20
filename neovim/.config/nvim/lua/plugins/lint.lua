75aa7e1100aca941e04eee6a7b163084ee825d92
Okan <okanakyuz@okanakyuz.com>
2025-06-20 18:31:57 +0300

    Cpplint error fix

--- neovim/.config/nvim/lua/plugins/lint.lua
+++ neovim/.config/nvim/lua/plugins/lint.lua
@@ -3,9 +3,15 @@ return {
     "nvimtools/none-ls.nvim",
     config = function()
       local null_ls = require("null-ls")
+
+      local mason_path = vim.fn.stdpath("data") .. "/mason/bin"
+
       null_ls.setup({
         sources = {
-          null_ls.builtins.diagnostics.cpplint,
+          null_ls.builtins.diagnostics.cpplint.with({
+            command = mason_path .. "/cpplint",
+            extra_args = { "--filter=-whitespace" },
+            }),
           null_ls.builtins.diagnostics.golangci_lint,
           null_ls.builtins.diagnostics.clippy, -- rust linter
         },
