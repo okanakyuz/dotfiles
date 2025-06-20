4696761218846eec3b396b696b1a40194f1f0771
Okan <okanakyuz@okanakyuz.com>
2025-06-20 18:36:59 +0300

    Add tokyonight

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,11 @@
-return {
-  {
-    "dylanaraps/wal.vim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("wal")
-      vim.cmd([[
-        highlight Normal guibg=NONE ctermbg=NONE
-        highlight NormalNC guibg=NONE ctermbg=NONE
-        highlight SignColumn guibg=NONE ctermbg=NONE
-        highlight VertSplit guibg=NONE ctermbg=NONE
-        highlight StatusLine guibg=NONE ctermbg=NONE
-      ]])
-    end,
-  },
+{
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("tokyonight").setup({
+      transparent = true, 
+    })
+    vim.cmd("colorscheme tokyonight")
+  end,
 }
