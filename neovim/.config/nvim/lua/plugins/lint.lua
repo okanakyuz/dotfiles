4b08c2a76d3793604d8956e62f3670bb4bcf48b4
Okan <okanakyuz@okanakyuz.com>
2025-06-20 19:15:37 +0300

    none-ls-extra

--- neovim/.config/nvim/lua/plugins/lint.lua
+++ neovim/.config/nvim/lua/plugins/lint.lua
@@ -1,16 +1,16 @@
 return {
   {
     "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
     config = function()
       local null_ls = require("null-ls")
       local mason_path = vim.fn.stdpath("data") .. "/mason/bin"
 
       null_ls.setup({
         sources = {
-          null_ls.builtins.diagnostics.cpplint.with({
-            command = mason_path .. "/cpplint",
-            extra_args = { "--filter=-whitespace" },
-          }),
+          require("none-ls.diagnostics.cpplint"),
           null_ls.builtins.diagnostics.golangci_lint,
         },
       })
