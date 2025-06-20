b7688559094b5bc75e7bb76d77ad832a34253a4b
Okan <okanakyuz@okanakyuz.com>
2025-06-20 17:52:34 +0300

    Create treesitter.lua

---
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {
+          "lua", "go", "bash", "json", "yaml", "toml", "c", "cpp". "rust"
+        },
+      })
+    end,
+  },
+}
