a2eb47aa35a5f9dd0a3a6662864b424188428e10
Okan <okanakyuz@okanakyuz.com>
2025-06-20 17:49:48 +0300

    Update lazy.lua

--- neovim/.config/nvim/lua/core/lazy.lua
+++ neovim/.config/nvim/lua/core/lazy.lua
@@ -5,10 +5,11 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("plugins")
+require("lazy").setup({
+  { import = "plugins" },
+})
