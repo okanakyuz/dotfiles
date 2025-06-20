4406f135b585df14c645ae1f3ed53d3dafbc5293
Okan <okanakyuz@okanakyuz.com>
2025-06-20 19:09:27 +0300

    Cpplint

--- neovim/.config/nvim/lua/plugins/lint.lua
+++ neovim/.config/nvim/lua/plugins/lint.lua
@@ -1,17 +1,19 @@
 return {
-    {
+  {
     "nvimtools/none-ls.nvim",
     config = function()
-        local null_ls = require("null-ls")
+      local null_ls = require("null-ls")
+      local mason_path = vim.fn.stdpath("data") .. "/mason/bin"
 
-        null_ls.setup({
+      null_ls.setup({
         sources = {
-            null_ls.builtins.diagnostics.cpplint.with({
+          null_ls.builtins.diagnostics.cpplint.with({
+            command = mason_path .. "/cpplint",
             extra_args = { "--filter=-whitespace" },
-            }),
-            null_ls.builtins.diagnostics.golangci_lint,
+          }),
+          null_ls.builtins.diagnostics.golangci_lint,
         },
-        })
+      })
     end,
-    }
+  },
 }
