5fbf60612c6e7fa38568e6412bcf6bc89229cecf
Okan <okanakyuz@okanakyuz.com>
2025-06-20 18:26:28 +0300

    Adding DAP-UI

--- neovim/.config/nvim/lua/core/keymaps.lua
+++ neovim/.config/nvim/lua/core/keymaps.lua
@@ -1,1 +1,13 @@
+local dap = require("dap")
+local dapui = require("dapui")
 
+vim.keymap.set("n", "<F5>", dap.continue, { desc = "DAP: Continue" })
+vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, { desc = "DAP: Toggle Breakpoint" })
+vim.keymap.set("n", "<F10>", dap.step_over, { desc = "DAP: Step Over" })
+vim.keymap.set("n", "<F11>", dap.step_into, { desc = "DAP: Step Into" })
+vim.keymap.set("n", "<F12>", dap.step_out, { desc = "DAP: Step Out" })
+
+vim.keymap.set("n", "<Leader>dr", dap.run_to_cursor, { desc = "DAP: Run to Cursor" })
+vim.keymap.set("n", "<Leader>ds", dap.restart, { desc = "DAP: Restart" })
+vim.keymap.set("n", "<Leader>dq", dap.terminate, { desc = "DAP: Terminate" })
+vim.keymap.set("n", "<Leader>du", dapui.toggle, { desc = "DAP: Toggle UI" })
--- neovim/.config/nvim/lua/plugins/dap.lua
+++ neovim/.config/nvim/lua/plugins/dap.lua
@@ -1,8 +1,31 @@
 return {
   {
     "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+    },
     config = function()
       local dap = require("dap")
+      local dapui = require("dapui")
+
+      -- DAP UI setup
+      dapui.setup({
+        floating = {
+          border = "rounded",
+        },
+      })
+
+    -- DAP UI automatic
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+
 
       -- C / C++
       dap.adapters.cppdbg = {
