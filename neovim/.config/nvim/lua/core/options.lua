fdf5f499846fb10a45f5d23380174d3ddfeeae88
Okan <okanakyuz@okanakyuz.com>
2025-06-20 17:47:01 +0300

    Create options.lua

---
+++ neovim/.config/nvim/lua/core/options.lua
@@ -0,0 +1,6 @@
+vim.opt.expandtab = false      
+vim.opt.tabstop = 4           
+vim.opt.shiftwidth = 4        
+vim.opt.softtabstop = 4
+vim.opt.number = true         
+vim.opt.relativenumber = true 
