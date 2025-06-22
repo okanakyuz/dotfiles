34b031f6067ca1c95bb4149cb1baa446a94e6534
Okan <okanakyuz@okanakyuz.com>
2025-06-23 02:21:00 +0300

    Fix git.lua

--- neovim/.config/nvim/lua/plugins/git.lua
+++ neovim/.config/nvim/lua/plugins/git.lua
@@ -5,18 +5,18 @@ return {
     config = function()
       require("neogit").setup({})
     end,
-  }
+  },
   {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
     end,
-  }
+  },
   {
     "sindrets/diffview.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("diffview").setup()
     end,
-  }
+  },
 }
