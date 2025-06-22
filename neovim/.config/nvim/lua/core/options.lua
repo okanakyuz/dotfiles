d574bded7844eafc12c7bf08430d308f81f8bdfd
Okan <okanakyuz@okanakyuz.com>
2025-06-22 23:13:14 +0300

    Update options.lua for severnity

--- neovim/.config/nvim/lua/core/options.lua
+++ neovim/.config/nvim/lua/core/options.lua
@@ -7,8 +7,11 @@ vim.opt.relativenumber = true
 
 
 vim.diagnostic.config({
-  underline = false,
-  virtual_text = false, 
+  underline = {
+    severity = { min = vim.diagnostic.severity.WARN },
+  },
+  virtual_text = false,
   signs = true,
   update_in_insert = false,
+  severity_sort = true,
 })
