407aee9c024cad0b12626b3ac4781a8f04e280e8
Okan <okanakyuz@okanakyuz.com>
2025-06-23 00:18:43 +0300

    Add Telescope fzf

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,15 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = {
-      "nvim-lua/plenary.nvim"
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        cond = vim.fn.executable("make") == 1,
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      },
     },
     config = function() 
       require("telescope").setup {
