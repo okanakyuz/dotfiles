6f1313b9f93b90ed09f099c801f671458da08eff
Okan <okanakyuz@okanakyuz.com>
2025-06-22 23:05:46 +0300

    Update diagnostics

--- neovim/.config/nvim/lua/core/options.lua
+++ neovim/.config/nvim/lua/core/options.lua
@@ -4,3 +4,11 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.number = true         
 vim.opt.relativenumber = true 
+
+
+vim.diagnostic.config({
+  underline = false,
+  virtual_text = false, 
+  signs = true,
+  update_in_insert = false,
+})
