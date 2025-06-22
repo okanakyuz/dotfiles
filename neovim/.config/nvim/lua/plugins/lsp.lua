45cbc573bc202e73fbe2036d4d6c40f6d519e750
Okan <okanakyuz@okanakyuz.com>
2025-06-22 23:16:48 +0300

    Define Capabilities

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -3,10 +3,17 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
+
       lspconfig.clangd.setup({
         capabilities = require("cmp_nvim_lsp").default_capabilities(),
-        cmd = { "clangd", "--header-insertion=never", "--inlay-hints=false" }, 
+        cmd = { "clangd", "--header-insertion=never" },
+        on_attach = function(client, bufnr)
+          if client.server_capabilities.inlayHintProvider then
+            vim.lsp.inlay_hint.enable(bufnr, false)
+          end
+        end,
       })
+
       lspconfig.rust_analyzer.setup({
             settings = {
                 ["rust-analyzer"] = {
