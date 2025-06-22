0637d2c060175d53b69ab5b24666596fd5049f9c
Okan <okanakyuz@okanakyuz.com>
2025-06-23 01:17:39 +0300

    Update lsp.lua

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -3,16 +3,6 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-
-      lspconfig.rust_analyzer.setup({
-            settings = {
-                ["rust-analyzer"] = {
-                checkOnSave = {
-                    command = "clippy"
-                }
-                }
-            }
-        })
     end,
   },
 }
