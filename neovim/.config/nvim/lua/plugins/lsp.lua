1e62cdea024e2df7686e00159d2590ea95bfd4bf
Okan <okanakyuz@okanakyuz.com>
2025-06-22 23:00:52 +0300

    Update lsp.lua Disable inlay-hint

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -3,7 +3,10 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.clangd.setup({})
+      lspconfig.clangd.setup({
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        cmd = { "clangd", "--header-insertion=never", "--inlay-hints=false" }, 
+      })
       lspconfig.rust_analyzer.setup({
             settings = {
                 ["rust-analyzer"] = {
