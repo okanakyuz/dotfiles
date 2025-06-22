f8367601bc7301025fa171967d5cc16845834d2f
Okan <okanakyuz@okanakyuz.com>
2025-06-23 02:18:17 +0300

    Create git.lua

---
+++ neovim/.config/nvim/lua/plugins/git.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "TimUntersberger/neogit",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neogit").setup({})
+    end,
+  }
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end,
+  }
+  {
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("diffview").setup()
+    end,
+  }
+}
