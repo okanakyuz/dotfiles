b5ec831276ef73e019214b52bbe2d62a94c22f33
Okan <okanakyuz@okanakyuz.com>
2025-06-23 10:22:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 sudo pacman -Syu --noconfirm
 sudo pacman -Sy --noconfirm base-devel git xorg-server xorg-xinit xorg-xrandr libx11 libxft libxinerama ttf-firacode-nerd python-pywal xorg-xrdb xorg-xsetroot xcompmgr 
-sudo pacman -Sy --noconfirm zsh exa bat fzf tmux ranger fd ripgrep htop lazygit unzip go rustup gdb fzf
+sudo pacman -Sy --noconfirm zsh exa bat fzf tmux ranger fd ripgrep htop lazygit unzip go rustup gdb 
 sudo pacman -Sy --noconfirm emacs 
-sudo pacman -Sy --noconfirm ranger ueberzug python-pygments bat
-sudo pacman -Sy --noconfirm zsh starship zoxide
+sudo pacman -Sy --noconfirm ranger ueberzug python-pygments 
+sudo pacman -Sy --noconfirm zsh starship zoxide bat fzf
 
 
 stow src
